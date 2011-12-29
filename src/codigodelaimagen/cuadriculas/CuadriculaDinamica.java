934fbe6e643fb34331daa6b30c9d3bd02535df25
juanantonioruz <juanantonioruz@gmail.com>
2011-12-29 02:47:59 +0100

    celdas y celdas sensibles

--- src/codigodelaimagen/cuadriculas/CuadriculaDinamica.java
+++ src/codigodelaimagen/cuadriculas/CuadriculaDinamica.java
@@ -9,7 +9,7 @@ public class CuadriculaDinamica extends CDIBase {
 
 	public void setup() {
 		super.setup();
-		 fila = creaCeldas(5, 10, 10, width-20, 100);
+		 fila = creaCeldas(5, 100, 10, width-200, 200);
 
 	}
 
@@ -27,12 +27,15 @@ public class CuadriculaDinamica extends CDIBase {
 	// 5=1+2+4+8+16=31
 	FilaRet fila;
 	public void draw() {
-		noLoop();
 		stroke(0);
 		strokeWeight(1);
 		stroke(color(30, 50, 80));
-		for(CeldaRet celda:fila.celdas)
-		rect(celda.x1, celda.y1, celda.x2-celda.x1, celda.y2);
+		
+		for(CeldaRet celda:fila.sensibles){
+			fill(100);
+			if(celda.sel) fill(40,50,100);
+			rect(celda.getX1(), celda.y1, celda.getAncho(), celda.getAlto());
+		}
 	}
 
 	
@@ -87,7 +90,9 @@ public class CuadriculaDinamica extends CDIBase {
 					MarcaPosicion marcaSig = marcas.get(i+1);
 					stroke(random(30,70));
 					noFill();
-					celdas.add(new CeldaRet(marcaActual.coordenada_x, marcaActual.coordenada_y,marcaSig.coordenada_x, marcaSig.coordenada_y+heightTotal));
+					CeldaRet celdaAnterior=null;
+					if(celdas.size()>0)celdaAnterior=celdas.get(celdas.size()-1);
+					celdas.add(new CeldaRet(marcaActual.coordenada_x, marcaActual.coordenada_y,marcaSig.coordenada_x, marcaSig.coordenada_y+heightTotal, celdaAnterior));
 					
 				}else{
 					//es el ultimo modulo se dibuja el rect desde el anterior
@@ -99,39 +104,43 @@ public class CuadriculaDinamica extends CDIBase {
 		}
 
 		public void raton(int mouseX, int mouseY) {
-			for(CeldaRet celda:fila.celdas){
-				if(mouseX>celda.x1 && mouseX<celda.x2 && mouseY>celda.y1 && mouseY<celda.y2){
-					log.info("encima de celda:"+celda);
+			sensibles=new ArrayList<CeldaRet>();
+			sensibles.addAll(celdas);
+			for(int i=0; i<sensibles.size(); i++){
+				CeldaRet celda=fila.sensibles.get(i);
+				if(mouseX>celda.getX1() && mouseX<celda.x2 && mouseY>celda.y1 && mouseY<celda.getY2()){
+					celda.sel=true;
+					// todo: order sensibles
+					log.info("celda pos sel: "+i);
+					if(i==0){
+						log.info("la primera");
+						// todo order por la primera
+					}else if (i==sensibles.size()-1){
+						log.info("la ultima");
+						// todo reverse order
+//						for(int j=0; j<celdas.size(); j++){
+//							CeldaRet celdaRet=celdas.get(j);
+//							CeldaRet celdaSen=sensibles.get(celdas.size()-j-1);
+////							celdaSen.y1
+//						}
+					}else{
+						log.info("en la mitad");
+					}
+				}else{
+					celda.sel=false;
+					// todo order por la primera
 				}
 			}
 		}
 
 		List<MarcaPosicion> marcas=new ArrayList<MarcaPosicion>();
 		List<CeldaRet> celdas=new ArrayList<CeldaRet>();
+		List<CeldaRet> sensibles=new ArrayList<CeldaRet>();
+
 
 	}
 	
-	class CeldaRet{
-
-		public final float x1;
-		public final float y1;
-		public final float x2;
-		public final float y2;
-
-		public CeldaRet(float x1, float y1, float x2, float y2) {
-			this.x1 = x1;
-			this.y1 = y1;
-			this.x2 = x2;
-			this.y2 = y2;
-		}
-
-		@Override
-		public String toString() {
-			return "CeldaRet [x1=" + x1 + ", y1=" + y1 + "]";
-		}
-		
-	}
-	class MarcaPosicion{
+		class MarcaPosicion{
 
 		public final float coordenada_x;
 		public  final float coordenada_y;
@@ -170,5 +179,48 @@ public class CuadriculaDinamica extends CDIBase {
 		super.ponsize(600, 600);
 	}
 
+	
+}
+class CeldaRet{
+
+	private final float x1;
+	public final float y1;
+	public final float x2;
+	private final float y2;
+	public boolean sel;
+	private final float ancho;
+	private final float alto;
+	public CeldaRet parent;
+	public CeldaRet(float x1, float y1, float x2, float y2, CeldaRet parent) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.parent = parent;
+		ancho=x2-x1;
+		alto=y2-y1;
+	}
+
+	public float getX1() {
+		if(parent!=null) return parent.getX1()+parent.getAncho();
+		return x1;
+	}
+	public float getY2() {
+		if(parent!=null) return parent.getY2()+parent.getAlto();
+		return y2;
+	}
+
+	public float getAncho() {
+		return ancho;
+	}
+	public float getAlto() {
+		return alto;
+	}
+
+	@Override
+	public String toString() {
+		return "CeldaRet [x1=" + x1 + ", y1=" + y1 + "]";
+	}
+	
 	
 }
