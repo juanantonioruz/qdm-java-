fbd3a82257c75c3c38f0172f46de3a4a35b62a73
juanantonioruz <juanantonioruz@gmail.com>
2011-12-29 04:44:18 +0100

    falta calculo de sobrante

--- src/codigodelaimagen/cuadriculas/CuadriculaDinamica.java
+++ src/codigodelaimagen/cuadriculas/CuadriculaDinamica.java
@@ -14,16 +14,14 @@ public class CuadriculaDinamica extends CDIBase {
 
 	public void setup() {
 		super.setup();
-		 fila = creaCeldas(5, 10, 10, width-20, 200);
+		fila = creaCeldas(5, 100, 10, width - 200, 200);
 
 	}
-
 
 	@Override
 	public void mouseMoved() {
 		fila.raton(mouseX, mouseY);
 	}
-
 
 	// 1=1
 	// 2=1+2=3
@@ -31,30 +29,27 @@ public class CuadriculaDinamica extends CDIBase {
 	// 4=1+2+4+8=15
 	// 5=1+2+4+8+16=31
 	FilaRet fila;
+
 	public void draw() {
-//		noLoop();
+		// noLoop();
 		background(100);
 		stroke(0);
 		strokeWeight(2);
 		noFill();
-		for(CeldaRet celda:fila.celdas){
+		for (CeldaRet celda : fila.celdas) {
 			fill(100);
-			if(celda.sel) fill(celda.color);
+			if (celda.sel)
+				fill(celda.color);
 			stroke(celda.color);
-			rect(fila.getCoordenada_x()+celda.getX1(), celda.y1, celda.getAncho(), celda.getAlto());
+			rect(fila.getCoordenada_x() + celda.getX1(), celda.y1, celda.getAncho(), celda.getAlto());
 		}
 	}
 
-	
-	
-	
-	private FilaRet creaCeldas(int numeroCeldas, float coordenada_x,float coordenada_y,  float widthTotal, float heightTotal) {
-		FilaRet fila=new FilaRet(numeroCeldas,coordenada_x, coordenada_y,  widthTotal, heightTotal, this);
+	private FilaRet creaCeldas(int numeroCeldas, float coordenada_x, float coordenada_y, float widthTotal,
+			float heightTotal) {
+		FilaRet fila = new FilaRet(numeroCeldas, coordenada_x, coordenada_y, widthTotal, heightTotal, this);
 		return fila;
 	}
-
-	
-
 
 	protected void ponBackground(int colorito) {
 		super.ponBackground(color(100));
@@ -62,38 +57,35 @@ public class CuadriculaDinamica extends CDIBase {
 
 	@Override
 	protected void ponsize(int i, int j) {
-		super.ponsize(600, 600);
+		super.ponsize(600, 300);
 	}
 
-	
 }
-class CeldaRet{
+
+class CeldaRet {
 	public boolean sel;
 
 	public final float y1;
 	private final float y2;
-	private  float ancho;
-	private  float anchoInicial;
+	private float ancho;
+	private float anchoInicial;
 	private float alto;
 	public CeldaRet parent;
-	
+
 	int color;
 
 	final FilaRet fila;
 
-
-	public CeldaRet(float y1,  float y2, CeldaRet parent, FilaRet fila, int color, float anchoInicial) {
+	public CeldaRet(float y1, float y2, CeldaRet parent, FilaRet fila, int color, float anchoInicial) {
 		this.y1 = y1;
 		this.y2 = y2;
 		this.parent = parent;
 		this.fila = fila;
 		this.color = color;
 		this.anchoInicial = anchoInicial;
-		ancho=anchoInicial;
-		alto=y2-y1;
+		ancho = anchoInicial;
+		alto = y2 - y1;
 	}
-
-
 
 	public void setAncho(float ancho) {
 		this.ancho = ancho;
@@ -102,18 +94,17 @@ class CeldaRet{
 	public void setAlto(float alto) {
 		this.alto = alto;
 	}
+
 	public float getX1() {
 		Calculo calculo = new Calculo();
-		float res=calculo.calcula(this);
+		float res = calculo.calcula(this);
 		return res;
 	}
-	
 
 	public float getY2() {
 		return y2;
 	}
 
-	
 	public float getAnchoInicial() {
 		return anchoInicial;
 	}
@@ -121,17 +112,15 @@ class CeldaRet{
 	public float getAncho() {
 		return ancho;
 	}
+
 	public float getAlto() {
 		return alto;
 	}
 
-
-	
-	
 }
-class FilaRet{
-	
-	
+
+class FilaRet {
+
 	private final float heightTotal;
 	private final int numeroCeldas;
 	private final float coordenada_x;
@@ -144,7 +133,9 @@ class FilaRet{
 	public float getCoordenada_x() {
 		return coordenada_x;
 	}
-	public FilaRet( int numeroCeldas, float coordenada_x, float coordenada_y, float widthTotal, float heightTotal, PApplet p5) {
+
+	public FilaRet(int numeroCeldas, float coordenada_x, float coordenada_y, float widthTotal, float heightTotal,
+			PApplet p5) {
 		super();
 
 		this.numeroCeldas = numeroCeldas;
@@ -158,11 +149,12 @@ class FilaRet{
 		// marca 1 (inicio)
 		log.info("add moduloRect");
 		marcas.add(new MarcaPosicion(coordenada_x, coordenada_y));
-		
-//		log.info("celdas: " + numeroCeldas + ". numeroDivisionesReticula: " + fila.numeroDivisionesReticula);
 
-		float fin=coordenada_x + heightTotal;
-		float inicioColumna_x=coordenada_x;
+		// log.info("celdas: " + numeroCeldas + ". numeroDivisionesReticula: " +
+		// fila.numeroDivisionesReticula);
+
+		float fin = coordenada_x + heightTotal;
+		float inicioColumna_x = coordenada_x;
 		for (int c = numeroCeldas; c > 0; c--) {
 			if (c > 1) {
 				int multiplicador = dame2((c - 1), 1);
@@ -174,26 +166,28 @@ class FilaRet{
 				marcas.add(new MarcaPosicion(inicioColumna_x + medidaModulo, coordenada_y));
 			}
 		}
-		
-		
-		for(int i=0; i<marcas.size(); i++){
-			if(i<marcas.size()-1){
-				MarcaPosicion marcaActual = marcas.get(i);
-				MarcaPosicion marcaSig = marcas.get(i+1);
-				float anchoInicial=marcaSig.coordenada_x-marcaActual.coordenada_x;
 
-				CeldaRet celdaAnterior=null;
-				if(i>0)celdaAnterior=celdas.get(i-1);
-				
-				celdas.add(new CeldaRet(marcaActual.coordenada_y,marcaSig.coordenada_y+heightTotal, celdaAnterior,this,  p5.color(p5.random(100), 100, 80),anchoInicial));
-				
-			}else{
-				//es el ultimo modulo se dibuja el rect desde el anterior
+		for (int i = 0; i < marcas.size(); i++) {
+			if (i < marcas.size() - 1) {
+				MarcaPosicion marcaActual = marcas.get(i);
+				MarcaPosicion marcaSig = marcas.get(i + 1);
+				float anchoInicial = marcaSig.coordenada_x - marcaActual.coordenada_x;
+
+				CeldaRet celdaAnterior = null;
+				if (i > 0)
+					celdaAnterior = celdas.get(i - 1);
+
+				celdas.add(new CeldaRet(marcaActual.coordenada_y, marcaSig.coordenada_y + heightTotal, celdaAnterior,
+						this, p5.color(p5.random(100), 100, 80), anchoInicial));
+
+			} else {
+				// es el ultimo modulo se dibuja el rect desde el anterior
 			}
-			
+
 		}
-		
+
 	}
+
 	private int extraNumeroDivisionesReticula(int numeroPosiciones) {
 		int res = 1;
 		for (int i = 1; i < numeroPosiciones; i++) {
@@ -210,79 +204,111 @@ class FilaRet{
 		}
 		return res;
 	}
-	
-	public void raton(int mouseX, int mouseY) {
-		for(int i=0; i<celdas.size(); i++){
-			CeldaRet celda=celdas.get(i);
-			float x1 = celda.fila.coordenada_x+celda.getX1();
-			if(mouseX>x1 && mouseX<(x1+celda.getAncho()) && mouseY>celda.y1 && mouseY<celda.getY2()){
-				celda.sel=true;
-				// todo: order sensibles
-				log.info("celda pos sel: "+i);
-				if(i==0){
-					log.info("la primera");
-					// todo order por la primera
 
-				}else if (i==celdas.size()-1){
+	public void raton(int mouseX, int mouseY) {
+		for (int i = 0; i < celdas.size(); i++) {
+			CeldaRet celda = celdas.get(i);
+			float x1 = celda.fila.coordenada_x + celda.getX1();
+			if (mouseX > x1 && mouseX < (x1 + celda.getAncho()) && mouseY > celda.y1 && mouseY < celda.getY2()) {
+				celda.sel = true;
+				// todo: order sensibles
+				log.info("celda pos sel: " + i);
+				if (i == 0) {
+					log.info("la primera");
+					resetInicial();
+
+				} else if (i == celdas.size() - 1) {
 					log.info("la ultima");
 					// todo reverse order
-					for(int j=0; j<celdas.size(); j++){
-						int indexSen = celdas.size()-j-1;
-						CeldaRet celdaRet=celdas.get(j);
-						CeldaRet celdaSen=celdas.get(indexSen);
-						log.info("indexSen"+indexSen+" index Ret:"+j);
+					for (int j = 0; j < celdas.size(); j++) {
+						int indexSen = celdas.size() - j - 1;
+						CeldaRet celdaRet = celdas.get(j);
+						CeldaRet celdaSen = celdas.get(indexSen);
 						celdaSen.setAncho(celdaRet.getAnchoInicial());
-//						celdaSen.y1
+						// celdaSen.y1
 					}
-				}else{
-					log.info("en la mitad");
-					for(int j=0; j<celdas.size(); j++){
-						CeldaRet celdaRet=celdas.get(j);
-						celdaRet.setAncho(celdaRet.getAnchoInicial());
+				} else if (celdas.size() > 2) {
+					// i es la posicion actual del bucle
+
+					// si es menor que dos valen las condiciones anteriores
+					log.info("en la mitad" + i);
+					float anchoBMitad = celdas.get(1).getAnchoInicial() / 2;
+					int restantesDcha = celdas.size() - i;
+					log.info("en la restantesDcha" + restantesDcha);
+					for (int j = 0; j < restantesDcha; j++) {
+						CeldaRet celdaInt = celdas.get(j + i);
+						CeldaRet celdaSig = celdas.get(j);
+
+						// TODO falta por calcular el sobrante y aplicarlo a los demas...
+						if (j == 1)
+							celdaInt.setAncho(celdaSig.getAnchoInicial() / 2);
+						else
+							celdaInt.setAncho(celdaSig.getAnchoInicial());
+						System.out.println(celdaInt);
+					}
+					int pos = 0;
+					for (int j = i - 1; j >= 0; j--) {
+						CeldaRet celdaInt = celdas.get(j);
+						pos++;
+						CeldaRet celdaSig = celdas.get(pos);
+						// TODO falta por calcular el sobrante y aplicarlo a los demas...
+						if (pos == 1)
+							celdaInt.setAncho(celdaSig.getAnchoInicial() / 2);
+						else
+							celdaInt.setAncho(celdaSig.getAnchoInicial());
+
 					}
 
+					int restantesIzq = i - 1;
 				}
-			}else{
-				celda.sel=false;
-				for(int j=0; j<celdas.size(); j++){
-					CeldaRet celdaRet=celdas.get(j);
-					celdaRet.setAncho(celdaRet.getAnchoInicial());
-				}
+			} else {
+				celda.sel = false;
+				// TODO : pasados unos segundos resetear --- resetInicial();
+
 			}
 		}
 	}
 
-	List<MarcaPosicion> marcas=new ArrayList<MarcaPosicion>();
-	 List<CeldaRet> celdas=new ArrayList<CeldaRet>();
+	private void resetInicial() {
+		for (int j = 0; j < celdas.size(); j++) {
+			CeldaRet celdaRet = celdas.get(j);
+			celdaRet.setAncho(celdaRet.getAnchoInicial());
+		}
+	}
 
+	List<MarcaPosicion> marcas = new ArrayList<MarcaPosicion>();
+	List<CeldaRet> celdas = new ArrayList<CeldaRet>();
 
 }
 
-	class MarcaPosicion{
+class MarcaPosicion {
 
 	public final float coordenada_x;
-	public  final float coordenada_y;
+	public final float coordenada_y;
 
 	public MarcaPosicion(float coordenada_x, float coordenada_y) {
 		this.coordenada_x = coordenada_x;
 		this.coordenada_y = coordenada_y;
 	}
-	
+
 }
-	class Calculo{
-		float res;
-		CeldaRet parent;
-		public float calcula(CeldaRet celdaRet) {
-			parent=celdaRet.parent;
-			sumaParent();
-			return res;
-		}
-		private void sumaParent() {
-			if(parent!=null) {
-				res+=parent.getAncho();
-				parent=parent.parent;
-				sumaParent();
-			}
-			
-		}
+
+class Calculo {
+	float res;
+	CeldaRet parent;
+
+	public float calcula(CeldaRet celdaRet) {
+		parent = celdaRet.parent;
+		sumaParent();
+		return res;
 	}
+
+	private void sumaParent() {
+		if (parent != null) {
+			res += parent.getAncho();
+			parent = parent.parent;
+			sumaParent();
+		}
+
+	}
+}
